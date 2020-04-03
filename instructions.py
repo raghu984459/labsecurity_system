bf3318729b9719d693ba51054e3d39f5b8c29757
raghu984 <gopavaram.raghunath.17002@iitgoa.ac.in>
2020-04-03 18:44:33 +0530

    My commit

---
+++ instructions.py
@@ -0,0 +1,15 @@
+# commands to set up the raspberry pi and necessary details
+
+Set up the bot father in telegram app and get the access token
+
+# install pyhton pip
+sudo apt-get install python-pip
+
+# install telepot
+sudo pip install telepot
+
+# copy the code and run in python ide after connecting leds to raspberry pi
+
+
+
+
